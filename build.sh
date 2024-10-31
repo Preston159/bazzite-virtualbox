fa2420845071036001775bfaaff0cad2c7fd4ca6
Preston Petrie <preston159@users.noreply.github.com>
2024-10-30 20:37:39 -0500

    Group packages

--- build.sh
+++ build.sh
@@ -12,10 +12,13 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# virtualization
 rpm-ostree install edk2-ovmf
 rpm-ostree install libvirt
 rpm-ostree install qemu
-rpm-ostree install tigervnc-server
 rpm-ostree install virt-manager
+# remote access
+rpm-ostree install tigervnc-server
+# development
 curl -L -o /tmp/vscode-x64.rpm 'https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64'
 rpm-ostree install /tmp/vscode-x64.rpm
