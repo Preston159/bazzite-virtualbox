434cfa23092eaeec7ef769c037f80ead7d8ef5ad
Preston Petrie <ppetrie@nyb.com>
2024-11-04 10:56:08 -0600

    Don't install virtualization tools on Deck

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+BUILD_VER="$1"
+
 
 ### Install packages
 
@@ -12,16 +14,28 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# installs a given package on all versions
+install () {
+  rpm-ostree install "$1"
+}
+
+# installs a given package only on the desktop version
+install_desktop () {
+  if [[ "$BUILD_VER" = "desktop" ]]; then
+    rpm-ostree install "$1"
+  fi
+}
+
 # virtualization
-rpm-ostree install edk2-ovmf
-rpm-ostree install libvirt
-rpm-ostree install qemu
-rpm-ostree install virt-manager
+install_desktop edk2-ovmf
+install_desktop libvirt
+install_desktop qemu
+install_desktop virt-manager
 # remote access
 #rpm-ostree install tigervnc-server # currently non-functional
 # development
 curl -L -o /tmp/vscode-x64.rpm 'https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64'
-rpm-ostree install /tmp/vscode-x64.rpm
-rpm-ostree install docker-cli
+install /tmp/vscode-x64.rpm
+install_desktop docker-cli
 # cli
-rpm-ostree install zsh
+install zsh
