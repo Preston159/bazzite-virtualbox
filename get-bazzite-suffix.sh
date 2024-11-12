37be150188b5d29afd619f305654d5949531172e
Preston Petrie <ppetrie@nyb.com>
2024-11-12 10:51:52 -0600

    Get upstream bazzite version

--- get-bazzite-suffix.sh
+++ get-bazzite-suffix.sh
@@ -12,3 +12,8 @@ case "$flavor" in
 esac
 
 echo "suffix=$SUFFIX" >> $GITHUB_OUTPUT
+
+UPSTREAM="ghcr.io/ublue-os/bazzite$SUFFIX:latest"
+docker pull "$UPSTREAM"
+UPSTREAM_VER="$(docker inspect "$UPSTREAM" | jq -r '.[0].Config.Labels["org.opencontainers.image.version"]')"
+echo "upstream_ver=$UPSTREAM_VER" >> $GITHUB_OUTPUT
